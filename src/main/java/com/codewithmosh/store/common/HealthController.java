c6c2727c92683b2e8622e1afd5bde3168a84a7d6
Tobias <1179071115@qq.com>
2026-01-02 23:00:45 +0800

    create health_controller

---
+++ src/main/java/com/codewithmosh/store/common/HealthController.java
@@ -0,0 +1,19 @@
+package com.codewithmosh.store.common;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, String>> health() {
+        return ResponseEntity.ok(Map.of("status", "ok"));
+    }
+}
+
